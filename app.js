249b546698197c60a4ae27c3b651ec89aa20a4c4
valencz <89651139+TinoCZ@users.noreply.github.com>
2026-04-01 18:20:12 -0300

    subiendo arreglos del navbar

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const yellow = document.querySelector(".yellow");
 const neon = document.getElementById("neonText");
 const toggle = document.getElementById("menuToggle");
 const nav = document.getElementById("navLinks");
-
+const navLinks = document.querySelectorAll("#navLinks a");
 
 setInterval(() => {
   pink.style.textShadow = `0 0 ${40 + Math.random()*20}px #ff00ff`;
@@ -22,4 +22,10 @@ rotateNeon();
 
 toggle.addEventListener("click", () => {
   nav.classList.toggle("active");
+});
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
 });
